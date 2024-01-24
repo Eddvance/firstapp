d1105389543bc4bc71184b799e90eb2662a31431
Edouard Leroy <leroyedouard@protonmail.com>
2024-01-24 17:25:34 +0100

    Methode de soumission POST!

--- src/main/java/com/practices/firstapp/controller/HelloServlet.java
+++ src/main/java/com/practices/firstapp/controller/HelloServlet.java
@@ -5,6 +5,9 @@ package com.practices.firstapp.controller;
 import dto.Personne;
 
 import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
@@ -15,9 +18,10 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        //Personne michaelSchumacher = new Personne("Michael","Schumacher");
-        //out.print("<HTML><BODY>مرحبا "+michaelSchumacher.getFullName()+" </BODY></HTML>");
-        out.println("<html><body><h1>Cours Java</h1></body></html>");
-        out.println("<a href=\"somme\">Accès au test somme</a></br>");
+        Personne michaelSchumacher = new Personne("Michael","Schumacher");
+        out.print("<HTML><BODY>مرحبا  "+michaelSchumacher.getFullName());
+        out.println("<a href=\"somme\">   - Accès au test somme</a>");
+        out.println("<a href=\"login\">  -  Accès identification</a>");
+        out.println("</BODY></HTML>");
     }
 }
