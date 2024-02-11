353b26b0818614fd668409b1e1c561b003906172
Edouard Leroy <leroyedouard@protonmail.com>
2024-02-11 19:29:13 +0100

    Design pattern MVC Architectures standards et technologies associees- intro a JEE

--- src/main/java/com/practices/firstapp/controller/SommeServlet.java
+++ src/main/java/com/practices/firstapp/controller/SommeServlet.java
@@ -17,29 +17,29 @@ public class SommeServlet extends HttpServlet {
 
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
 
 
-        String nombre1 = req.getParameter("nombre1");
-        String nombre2 = req.getParameter("nombre2");
+        String nombre1 = request.getParameter("nombre1");
+        String nombre2 = request.getParameter("nombre2");
 
         try {
             int somme = Integer.parseInt(nombre1) + Integer.parseInt(nombre2);
 
-            String format = req.getParameter("format");
+            String format = request.getParameter("format");
+            request.setAttribute("somme", somme);
             if (format != null && format.equals("pdf")) {
-                req.setAttribute("somme", somme);
-                RequestDispatcher disp = req.getRequestDispatcher("/pdf");
-                disp.forward(req, resp);
+                RequestDispatcher disp = request.getRequestDispatcher("/pdf");
+                disp.forward(request, resp);
             } else {
-                resp.setContentType("text/html;charset=UTF-8");
-                PrintWriter out = resp.getWriter();
-                out.println("<html><body><a href=\"saisie-nombre.html\">Cliquez ici pour effectuer un nouveau calcul</a><br/>");
-                out.print("La somme des 2 nombres fournis est : " + somme + "</body></html>");
+                RequestDispatcher disp = request.getRequestDispatcher("/affichage-somme.jsp");
+                disp.forward(request, resp);
+               //("<html><body><a href=\"saisie-nombre.html\">Cliquez ici pour effectuer un nouveau calcul</a><br/>");
+               //("La somme des 2 nombres fournis est : " + somme + "</body></html>");
             }
         }
             catch (NumberFormatException nfe) {
-                RequestDispatcher disp = req.getRequestDispatcher("/unexpected-error.html");
+                RequestDispatcher disp = request.getRequestDispatcher("/unexpected-error.html");
             }
     }
 }
---
+++ src/main/webapp/affichage-somme.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: eddbea
+  Date: 11/02/2024
+  Time: 7:18 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Somme</title>
+</head>
+<body>
+La somme des 2 nombres fournis est : ${somme}
+</body>
+</html>
