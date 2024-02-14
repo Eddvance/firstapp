f45a200255f216546d683aadae7373daf4ec1d64
Edouard Leroy <leroyedouard@protonmail.com>
2024-02-14 19:39:51 +0100

    JSTL

--- src/main/java/com/practices/firstapp/controller/SelectionLivreServlet.java
+++ src/main/java/com/practices/firstapp/controller/SelectionLivreServlet.java
@@ -12,6 +12,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.swing.text.html.CSS.getAttribute;
 
 @WebServlet(name = "SelectionLivreServlet", value = "/selection-livre")
 public class SelectionLivreServlet extends HttpServlet {
@@ -21,13 +25,20 @@ public class SelectionLivreServlet extends HttpServlet {
         HttpSession session=request.getSession();
         String numeroSession=session.getId();
         String identifiantLivre=request.getParameter("id");
-        Livre livre=new Livre();
-        livre.setNumeroLivre(Integer.parseInt(identifiantLivre));
-        session.setAttribute("livre", livre);
 
+        List<Livre> liste=(List<Livre>)session.getAttribute("listLivres");
+
+        if(liste==null){
+            liste=new ArrayList<>();
+            session.setAttribute("listeLivre", liste);
+        }
+
+        Livre livre=new Livre();
+        livre.setNumeroLivre(Integer.parseInt((identifiantLivre)));
+        liste.add(livre);
         PrintWriter out=resp.getWriter();
         out.println("<html><body>Merci d'avoir choisi le livre "+identifiantLivre+"<br/>");
-        out.println("Votre numero de session est : "+numeroSession+".<br/>");
+        out.println("<a href=\"choix-du-livre.html\"> Cliquez ici pour ajouter un autre livre</a><br/>");
         out.println("<a href=\"payer-livre.html\">Cliquez ici pour regler</a></body></html><br/>");
 
     }
--- src/main/webapp/payer-livre.jsp
+++ src/main/webapp/payer-livre.jsp
@@ -6,14 +6,17 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
 <head>
-    <title>Title</title>
+    <title>Livre paye</title>
 </head>
-<body>
-Paiement effectue avec le numero de carte suivant : <%=request.getParameter("numeroCarte")%><br/>
-<%--Le livre paye est le livre numero : <%=session.getAttribute("identifiantLivre")%><br/>--%>
-Le livre paye est le livre numero : ${sessionScope.livre.numeroLivre}<br/>
-<%--Le livre paye est le livre numero : ${livre.numeroLivre}<br/>--%>
-</body>
+    <body>
+        Paiement effectue avec le numero de carte suivant : <%=request.getParameter("numeroCarte")%><br/><%--Le livre paye est le livre numero : <%=session.getAttribute("identifiantLivre")%><br/>--%>
+        Liste des livres payes : <br/>
+        <c:forEach items="${listeLivres}" var="livre">
+        Le livre paye est le livre numero : ${livre.numeroLivre}<br/><%--Le livre paye est le livre numero : ${sessionScope.livre.numeroLivre}<br/>--%>
+        </c:forEach>
+    </body>
 </html>
